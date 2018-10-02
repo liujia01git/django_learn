e984e0695012afddfcf6b133c6b8494a6552c6e2
liujia01git <824957807@qq.com>
2018-10-02 22:24:58 +0800

    删除冗余代码

--- polls/admin.py
+++ polls/admin.py
@@ -4,19 +4,7 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-'''
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
-    ]
 
-admin.site.register(Question, QuestionAdmin)
-
-# ...
-admin.site.register(Choice)
-
-'''
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
